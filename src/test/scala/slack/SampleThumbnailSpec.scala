bf3ae20cf784029579f54d1bb539d1cd0503989d
nokamoto <nokamoto@hotmail.co.jp>
2017-02-24 23:36:07 +0900

    Add thumbnail generator

---
+++ src/test/scala/slack/SampleThumbnailSpec.scala
@@ -0,0 +1,31 @@
+package slack
+
+import java.time.ZonedDateTime
+
+import core.Context
+import helper.{DefaultFutures, DummyPullRequest}
+import org.scalatest.FlatSpec
+
+/**
+  * sbt "set testOptions in Test := Nil" "testOnly slack.SampleThumbnailSpec"
+  */
+class SampleThumbnailSpec extends FlatSpec with DefaultFutures with DummyPullRequest {
+  it should "post a sample message for thumbnail" taggedAs SlackTest in {
+    val context = new Context(
+      now = ZonedDateTime.now(), github = Context.github, slack = Context.slack.copy(iconEmoji = Some(":octocat:")))
+
+    val service = new SlackService(context)
+
+    import context._
+
+    val pulls = pullRequest(createdAt = now.minusDays(1), number = 12) ::
+      pullRequest(createdAt = now.minusDays(slack.warningAfter.toDays), number = 11, comments = 8) ::
+      pullRequest(createdAt = now.minusDays(slack.dangerAfter.toDays), number = 10, comments = 32) :: Nil
+
+    try {
+      whenReady(service.webhook(pulls))(_ => ())
+    } finally {
+      context.shutdown()
+    }
+  }
+}
